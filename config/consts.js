624be8dedba88cdcc5d8b9b970a3d68c9c75e6f6
Felipe Castillo <felipecastillo48@gmail.com>
2026-04-25 22:50:01 -0400

    feat: b ind port to env

--- config/consts.js
+++ config/consts.js
@@ -1,26 +1,26 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config({quiet:true})
+dotenv.config({ quiet: true });
 
-export const PORT = 3000
+export const PORT = process.env.PORT || 3000;
 
 export const DB_CONFIG = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-}
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
 
 export const EMAIL_CONFIG = {
-    host: process.env.MAIL_HOST || 'smtp.resend.com',
-    port: process.env.MAIL_PORT || 587,
-    user: process.env.MAIL_USER,
-    password: process.env.MAIL_PASS,
-    from: process.env.MAIL_FROM || 'DataField <onboarding@resend.dev>'
-}
+  host: process.env.MAIL_HOST || "smtp.resend.com",
+  port: process.env.MAIL_PORT || 587,
+  user: process.env.MAIL_USER,
+  password: process.env.MAIL_PASS,
+  from: process.env.MAIL_FROM || "DataField <onboarding@resend.dev>",
+};
 export const CLOUDINARY_CONFIG = {
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-}
+};
